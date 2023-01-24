ac20deec63b4ae04701f0b25044c2de55e007683
Tyler Imbo <tyler.imbo@arizent.com>
2023-01-23 22:14:18 -0500

    data added

---
+++ borderless_tourist/script.py
@@ -0,0 +1,9 @@
+destinations = [
+    "Paris, France",
+    "Shanghai, China",
+    "Los Angeles, USA",
+    "São Paulo, Brazil",
+    "Cairo, Egypt",
+]
+
+test_traveler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
