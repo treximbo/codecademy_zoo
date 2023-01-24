562dbc38e0cc4ee2917934c91918fae48b900202
Tyler Imbo <tyler.imbo@arizent.com>
2023-01-23 22:34:17 -0500

    functions added

--- borderless_tourist/script.py
+++ borderless_tourist/script.py
@@ -7,3 +7,18 @@ destinations = [
 ]
 
 test_traveler = ["Erin Wilkes", "Shanghai, China", ["historical site", "art"]]
+
+
+def get_destination_index(destination):
+    destination_index = destinations.index(destination)
+    return destination_index
+
+
+def get_traveler_location(traveler):
+    traveler_destination = traveler[1]
+    traveler_destination_index = get_destination_index(traveler_destination)
+    return traveler_destination_index
+
+
+test_destination_index = get_traveler_location(test_traveler)
+print(test_destination_index)
