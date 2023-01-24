40e82b91458080a9a8507e7a27169be29df35613
Tyler Imbo <tyler.imbo@arizent.com>
2023-01-23 23:20:33 -0500

    find attraction function

--- borderless_tourist/script.py
+++ borderless_tourist/script.py
@@ -34,3 +34,33 @@ def add_attraction(destination, attraction):
 
 
 add_attraction("Los Angeles, USA", ["Venice Beach", ["beach"]])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction(
+    "Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]]
+)
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+    for possible_attraction in attractions_in_city:
+        attraction_tags = possible_attraction[1]
+        for interest in interests:
+            if attraction_tags.count(interest) > 0:
+                attractions_with_interest.append(possible_attraction[0])
+            else:
+                continue
+    return attractions_with_interest
+
+
+la_arts = find_attractions("Los Angeles, USA", ["art"])
+print(la_arts)
